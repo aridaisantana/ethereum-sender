acacdf20cd69bc91c119d040475427889fb49418
Aridai Santana <aridai@secret-source.eu>
2022-01-04 11:28:50 +0000

    FrontEnd Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {TransactionProvider} from './context/TransactionContext';
 
 ReactDOM.render(
+  <TransactionProvider>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </TransactionProvider>,
   document.getElementById('root')
 );
 
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x2D78A9d9B11D58dB96B9718DE7A4F6656cfda86d';
